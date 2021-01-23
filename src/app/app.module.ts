81c3e6ab03a31a7fd3c9f0af4b8c9c9cb8f9bab4
Fabian Valencia <fabianmvr@gmail.com>
2021-01-22 22:35:20 -0500

    Creating visual plate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlateComponent } from './plate/plate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlateComponent
   ],
   imports: [
     BrowserModule,
